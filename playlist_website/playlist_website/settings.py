e0ea4b548ee71033ada1e201b9b54aa135599386
Issai Torres <issaitorres@gmail.com>
2020-11-25 21:28:18 -0800

    minor fixes

--- playlist_website/playlist_website/settings.py
+++ playlist_website/playlist_website/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xctz=5fe2mx75*0svpke8-@0#@w0$gly$1k)3i_2d2x(cu%xv8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
